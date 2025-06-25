91b1c52de7ef43cd5df5d3be80496a18bd47075c
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-06-25 10:51:22 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categoriesList = document.getElementById("categories");
+const categoryItems = categoriesList.querySelectorAll(".item");
+
+console.log(`Кількість категорій: ${categoryItems.length}`);
+
+categoryItems.forEach((item) => {
+  const title = item.querySelector("h2").textContent;
+  const elementsCount = item.querySelectorAll("ul > li").length;
+
+  console.log(`Категорія: ${title}`);
+  console.log(`Кількість елементів: ${elementsCount}`);
+});
